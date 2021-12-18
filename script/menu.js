aa7411519836fc2402e777e1fa399c75c1223268
nmelgar <nmelgar16013@gmail.com>
2021-12-17 19:11:17 -0600

    19:10--changes to menu button, not working fine

--- script/menu.js
+++ script/menu.js
@@ -1,5 +1,12 @@
-(document).ready(function() {
-    $('#menu-image').on('click', function() {
-      $('#navigation-list-small-js').show();
-    })
-  });
+
+let menuButton = document.getElementById('menu-button').addEventListener("click", displayNavMenu);
+
+function displayNavMenu() {
+  let navListSmall = document.getElementById('navigation-list-small-js');
+  if (navListSmall.style.display === "none") {
+    navListSmall.style.display = "grid";
+    // navListSmall.style.transition = "0.8s";
+  } else {
+    navListSmall.style.display = "none";
+  }
+}
