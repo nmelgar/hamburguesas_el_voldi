44b49582049dd8e7b01ee0a55ab63a71d0c5a1e8
nmelgar <nmelgar16013@gmail.com>
2022-01-04 13:07:38 -0600

    13:07--working nav-bars menu

--- elvoldi/menu/static/script/menu.js
+++ elvoldi/menu/static/script/menu.js
@@ -1,5 +1,17 @@
-(document).ready(function() {
-    $('#menu-image').on('click', function() {
-      $('#navigation-list-small-js').show();
-    })
-  });
+function dropDownFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
